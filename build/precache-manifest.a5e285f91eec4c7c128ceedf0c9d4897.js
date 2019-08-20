02a2501d27215fe5e734b914778e2a96491fd760
Ricardo <r.n.hori@gmail.com>
2019-08-20 15:30:21 -0300

    build completed

---
+++ build/precache-manifest.a5e285f91eec4c7c128ceedf0c9d4897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8715514ec800c29039566d5db2f7d9d3",
+    "url": "/Startup-ReactJS-WebDev-Test/index.html"
+  },
+  {
+    "revision": "0a0272769556f0009c0a",
+    "url": "/Startup-ReactJS-WebDev-Test/static/js/2.d7b4c85e.chunk.js"
+  },
+  {
+    "revision": "34829da09b5cfcefa26a",
+    "url": "/Startup-ReactJS-WebDev-Test/static/js/main.a199ea54.chunk.js"
+  },
+  {
+    "revision": "3ec732d08f02ac0c88a6",
+    "url": "/Startup-ReactJS-WebDev-Test/static/js/runtime~main.4262ab3f.js"
+  },
+  {
+    "revision": "c64606851276ca93be86c561a423895d",
+    "url": "/Startup-ReactJS-WebDev-Test/static/media/avatar.c6460685.png"
+  },
+  {
+    "revision": "8407d2c3adc2000e546c4f56f7093b02",
+    "url": "/Startup-ReactJS-WebDev-Test/static/media/logo.8407d2c3.png"
+  }
+]);
