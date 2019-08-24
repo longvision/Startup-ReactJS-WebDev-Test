31e12c60d0c559a278e10c9d73d3560eecafb4fa
Ricardo <r.n.hori@gmail.com>
2019-08-24 16:27:56 -0300

    git hub pages setup

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import Commits from './components/Commits';
-// import List from './components/List';
 
+//Change basename prop to send to production as on https://github.com/gitname/react-gh-pages/issues/3
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={Main} />
-    <Route path="/commits" component={Commits} />
+    <BrowserRouter basename="/Startup-ReactJS-WebDev-Test">
+      <Route exact path="/" component={Main} />
+      <Route path="/commits" component={Commits} />
+    </BrowserRouter>
   </Switch>
 );
 
